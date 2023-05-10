9d39ec6081c6e9beffdbb6fc49288775c767f12f
gianfrancobarba <gianfrancobarba25@gmail.com>
2023-05-10 17:02:21 +0200

    aggiornare il database con indirizzo new entity. modificato tutto. si deve aggiungere AdressControllo e AdressDAO

--- StorageSession/WebContent/SignForm.jsp
+++ StorageSession/WebContent/SignForm.jsp
@@ -46,36 +46,6 @@
         <div class="cut"></div>
         <label for="nome" class="placeholder">Password</label>
       </div>
-      <div class="input-container ic1">
-        <input id="citta" class="input" type="text" name="citta" placeholder=" " required>
-        <div class="cut"></div>
-        <label for="citta" class="placeholder">Citta'</label>
-      </div>
-      <div class="input-container ic1">
-        <input id="provincia" class="input" type="text" name="provincia" placeholder=" " required>
-        <div class="cut"></div>
-        <label for="provincia" class="placeholder">Provincia</label>
-      </div>
-      <div class="input-container ic1">
-        <input id="cap" class="input" type="text" name="cap" placeholder=" " required>
-        <div class="cut"></div>
-        <label for="cap" class="placeholder">CAP</label>
-      </div>
-      <div class="input-container ic1">
-        <input id="voa" class="input" type="text" name="via" placeholder=" " required>
-        <div class="cut"></div>
-        <label for="via" class="placeholder">Via</label>
-      </div>
-      <div class="input-container ic1">
-        <input id="numero_civico" class="input" type="text" name="numero_civico" placeholder=" " required>
-        <div class="cut"></div>
-        <label for="numero_civico" class="placeholder">Numero Civico</label>
-      </div>
-      <div class="input-container ic1">
-        <input id="stato" class="input" type="text" name="stato" placeholder=" " required>
-        <div class="cut"></div>
-        <label for="stato" class="placeholder">Stato</label>
-      </div>
 		<input type="submit" value="Registrati" class="submit">
 	</form>
 </div>
--- StorageSession/src/it/unisa/model/UserDaoImpl.java
+++ StorageSession/src/it/unisa/model/UserDaoImpl.java
@@ -17,8 +17,8 @@ public class UserDaoImpl implements UserDAO {
         int result;
 
         String insertSQL = "INSERT INTO " + UserDaoImpl.TABLE
-                           + " (nome, cognome, dataNascita, CF, numTelefono, email, psw, citta, provincia, cap, via, numCivico,"
-                           + "stato) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                           + " (nome, cognome, dataNascita, CF, numTelefono, email, psw"
+                           + "stato) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
         try {
             connection = DriverManagerConnectionPool.getConnection();
@@ -31,12 +31,7 @@ public class UserDaoImpl implements UserDAO {
             preparedStatement.setString(5, user.getNumTelefono());
             preparedStatement.setString(6, user.getEmail());
             preparedStatement.setString(7, user.getPassword());
-            preparedStatement.setString(8, user.getCitta());
-            preparedStatement.setString(9, user.getProvincia());
-            preparedStatement.setString(10, Integer.toString(user.getCap()));
-            preparedStatement.setString(11, user.getVia());
-            preparedStatement.setInt(12, user.getNumCivico());
-            preparedStatement.setString(13, user.getStato());
+
 
             result = preparedStatement.executeUpdate();
             
@@ -119,16 +114,10 @@ public class UserDaoImpl implements UserDAO {
                 user.setCodF(rs.getString("CF"));
                 user.setNome(rs.getString("nome"));
                 user.setCognome(rs.getString("cognome"));
-                user.setVia(rs.getString("via"));
-                user.setCitta(rs.getString("citta"));
-                user.setProvincia(rs.getString("provincia"));
-                user.setCap(Integer.parseInt(rs.getString("cap")));
                 user.setNumTelefono(rs.getString("numTelefono"));
-                user.setStato(rs.getString("stato"));
                 user.setId(Integer.parseInt(rs.getString("ID")));
                 user.setPassword(rs.getString("psw"));
                 user.setTipo(rs.getString("tipo"));
-                user.setNumCivico(Integer.parseInt(rs.getString("numCivico")));
                 user.setDataNascita(LocalDate.parse(rs.getDate("dataNascita").toString()));
                
             }
